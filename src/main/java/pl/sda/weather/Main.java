5f96e7630958c36a01af481ddab5168ffb60b3f2
kamil289@vp.pl <kamil289@vp.pl>
2022-02-28 12:36:48 +0100

    method to add and display location

--- src/main/java/pl/sda/weather/Main.java
+++ src/main/java/pl/sda/weather/Main.java
@@ -2,8 +2,6 @@ package pl.sda.weather;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
       Gui.showMainMenu();
 
--- src/main/java/pl/sda/weather/controllers/ControllersLocation.java
+++ src/main/java/pl/sda/weather/controllers/ControllersLocation.java
@@ -1,8 +1,55 @@
 package pl.sda.weather.controllers;
 
 
-public class ControllersLocation {
+import pl.sda.weather.model.LocationModel;
+import pl.sda.weather.service.ILocationService;
+import pl.sda.weather.service.impl.LocationServiceImpl;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
+
+import static pl.sda.weather.Gui.showMainMenu;
+
+public class ControllersLocation {
+    ILocationService locationService = new LocationServiceImpl();
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+    public  void addLocation() {
+
+        String idUUID = String.valueOf(UUID.randomUUID());
+
+        System.out.println("Write longitude and latitude :");
+        String longitudeAndLatitude = scanner.next();
+
+        System.out.println("Write city name : ");
+        String cityName = scanner.next();
+
+        System.out.println("Write region : ");
+        String region = scanner.next();
+
+        System.out.println("Write country name :");
+        String countryName = scanner.next();
+
+
+        locationService.addLocationModelToDB(new LocationModel(idUUID,longitudeAndLatitude,cityName,region,countryName));
+
+        showMainMenu();
+
+    }
+
+    public void displayLocation() {
+
+        List<LocationModel> list = locationService.getLocationModelFromFile();
+
+        for (LocationModel locationModel : list) {
+            System.out.println(locationModel);
+        }
+        showMainMenu();
+
+    }
 
 
 
